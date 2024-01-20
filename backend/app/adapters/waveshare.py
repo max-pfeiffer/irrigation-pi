32d20023b83d2f5940db76bbe7cfaea885e3b0e0
Max Pfeiffer <max@maxpfeiffer.ch>
2024-01-20 18:35:48 +0100

    Updated waveshare adapter, updated dependencies

---
+++ backend/app/adapters/waveshare.py
@@ -0,0 +1,31 @@
+from app.adapters.gpio_devices import Relay
+
+
+class WaveshareRpiRelayBoardAdapter:
+    """
+    Adapter for RPi Relay Board from Waveshare.
+    The Board carries three relays which are connected to GPIO pins.
+    https://www.waveshare.com/rpi-relay-board.htm
+    https://www.waveshare.com/wiki/RPi_Relay_Board
+    """
+    def __init__(self):
+        self.relays: list[Relay] = [Relay(26), Relay(20), Relay(21)]
+        self.relay_position_mapping: dict = {
+            1: 0,
+            2: 1,
+            3: 2,
+        }
+
+    def on(self, relay_position: int):
+        if relay_position not in self.relay_position_mapping.keys():
+            raise ValueError("Invalid relay position")
+
+        relay: Relay = self.relays[relay_position]
+        relay.on()
+
+    def off(self,relay_position: int):
+        if relay_position not in self.relay_position_mapping.keys():
+            raise ValueError("Invalid relay position")
+
+        relay: Relay = self.relays[relay_position]
+        relay.off()
