0313b3648e01791dc4d9ecc92300242f410f38d7
Max Pfeiffer <max@maxpfeiffer.ch>
2024-01-27 16:07:26 +0100

    Created script for exporting OpenAPI specs
    OpenAPI specs are exported in JSON format.
    Exported first API specs version.

---
+++ backend/utilities/export_api_specification.py
@@ -0,0 +1,24 @@
+"""Script for exporting backend API specification."""
+import json
+from pathlib import Path
+
+from app.main import app
+
+
+def main():
+    """Export API specifications as JSON.
+
+    :return:
+    """
+    api_specs: dict = app.openapi()
+    version: str = api_specs["info"]["version"].replace(".", "_")
+
+    api_specs_path: Path = (Path(__file__).parent.parent.resolve()
+                            / "api_specification" / f"api_specs_v{version}.json")
+
+    with open(api_specs_path, "w") as file:
+        file.write(json.dumps(api_specs))
+
+
+if __name__ == "__main__":
+    main()
