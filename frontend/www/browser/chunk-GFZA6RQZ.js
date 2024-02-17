82ee1afcf5dcc35fc2d7a33cd528929ac28d0ad2
Max Pfeiffer <max@maxpfeiffer.ch>
2024-02-17 11:26:13 +0100

    Added nginx and systemd config for Raspberry Pi installation
    nginx is used as web server for fronent and as reverse proxy
    for backend application.
    We control the backend application with systemd.

---
+++ frontend/www/browser/chunk-GFZA6RQZ.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-MTRQTMXR.js";import"./chunk-4SCXDPRD.js";import"./chunk-IWEF5KMV.js";import"./chunk-ML2ZISXZ.js";import"./chunk-LF5XB4YN.js";import"./chunk-PG5KTQWO.js";export{b as iosTransitionAnimation,a as shadow};
---
+++ frontend/www/browser/chunk-QRUHOFN4.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-Z4V4M3ZT.js";import"./chunk-PG5KTQWO.js";export{a as GESTURE_CONTROLLER,b as createGesture};
