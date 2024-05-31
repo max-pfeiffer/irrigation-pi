8becd23e27433503e583d20c506cee4aa016bf14
Moritz Pfeiffer <moritz@mp.mozmail.com>
2024-05-31 20:00:15 +0200

    feat(frontend): added relay menu item and list.

---
+++ frontend/src/app/relay/pages/list/relay-list.page.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonList,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+  ViewDidEnter,
+} from '@ionic/angular/standalone';
+import { Relay } from 'frontend/src/app/schedule/models/scheduler.models';
+import { RelayTileComponent } from '../../components/tile/relay-tile.component';
+import { RelayService } from '../../services/relay.service';
+
+@Component({
+  selector: 'app-relay-list',
+  templateUrl: './relay-list.page.html',
+  styleUrls: ['./relay-list.page.scss'],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonMenuButton,
+    IonTitle,
+    IonContent,
+    IonList,
+    RelayTileComponent,
+  ],
+})
+export class RelayListPage implements ViewDidEnter {
+  public relays: Relay[] = [];
+
+  constructor(
+    public relayService: RelayService,
+    public cdr: ChangeDetectorRef
+  ) {}
+
+  public ionViewDidEnter(): void {
+    this.refreshList();
+  }
+
+  public refreshList(): void {
+    this.relayService.getRelays().subscribe({
+      next: (_relays: Relay[]) => {
+        this.relays = _relays;
+        this.cdr.detectChanges();
+      },
+    });
+  }
+}
