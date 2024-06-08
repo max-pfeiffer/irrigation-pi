dc80615bc2e4a6b30a016e1ff631f86b2330fbfe
Max Pfeiffer <max@maxpfeiffer.ch>
2024-06-08 08:38:03 +0200

    Build the static files

---
+++ frontend/www/browser/chunk-ARTETJFA.js
@@ -0,0 +1,1 @@
+import{b as a,c as b}from"./chunk-JWIEPCRG.js";import"./chunk-PG5KTQWO.js";export{a as GESTURE_CONTROLLER,b as createGesture};
--- frontend/www/browser/chunk-GFZA6RQZ.js
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-MTRQTMXR.js";import"./chunk-4SCXDPRD.js";import"./chunk-IWEF5KMV.js";import"./chunk-ML2ZISXZ.js";import"./chunk-LF5XB4YN.js";import"./chunk-PG5KTQWO.js";export{b as iosTransitionAnimation,a as shadow};
